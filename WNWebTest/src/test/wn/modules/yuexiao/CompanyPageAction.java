1f5b59873190accf2aa3ef17560759c27f4456db
yuexiaocode <15011527023@163.com>
2016-04-08 17:42:21 +0800

    岳晓添加Company分页查询功能

--- WNWebTest/src/test/wn/modules/yuexiao/CompanyPageAction.java
+++ WNWebTest/src/test/wn/modules/yuexiao/CompanyPageAction.java
@@ -1,24 +1,51 @@
 package test.wn.modules.yuexiao;
 
+import com.weinong.base.ApiDefined;
 import com.weinong.base.ApiResult;
 import com.weinong.base.BaseApiPageAction;
+import test.wn.bean.Company;
+import test.wn.service.CompanyService;
 import yao.pagespliter.Page;
+import yao.util.log.Console;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Administrator on 2016/4/8.
  */
+
+
+@ApiDefined(label = "CompanyAction",description = "分页查询公司信息")
 public class CompanyPageAction extends BaseApiPageAction {
     @Override
     public ApiResult doPageApi() throws Exception {
 
+        try {
+            List<Company> companies = CompanyService.findPage(page);
 
-        return null;
+            return  result_success(1, companies);
+        }
+        catch (SQLException e){
+
+            Console.error(this,"分页查询公司失败",e);
+            result_error("分页查询公司失败",e);
+        }
+        return  result_fail(-1);
+
     }
 
     @Override
     protected void registResult(Map<Integer, String> map) {
+        map.put(1,"分页查询公司信息成功");
+        map.put(-1,"分页查询公司信息失败");
 
+    }
+
+    @Override
+    public String getStaticDescription() {
+        return "调用示例:<a target='_blank' href='company_page.wn?page={\"lastId\":5,\"pageSize\":2,\"sortType\":\"DESC\"}'>company_page.wn?page={\"lastId\":5,\"pageSize\":2,\"sortType\":\"DESC\"}</a>";
     }
 }
--- WNWebTest/src/test/wn/service/CompanyService.java
+++ WNWebTest/src/test/wn/service/CompanyService.java
@@ -1,8 +1,14 @@
 package test.wn.service;
 
+import com.weinong.base.IDPage;
 import test.wn.BaseService;
 import test.wn.bean.Company;
 import test.wn.core.TestDB;
+import yao.pagespliter.Page;
+import yao.util.db.bean.Limit;
+import yao.util.db.bean.OrderBy;
+import yao.util.db.bean.OrderItem;
+import yao.util.db.tool.Select;
 import yao.util.db.where.Expression;
 import yao.util.log.Console;
 
@@ -38,6 +44,15 @@ public class CompanyService extends BaseService {
 
     public static int update(Company company) throws SQLException {
         return TestDB.TestDB().getUpdate().updateSingle(company);
+    }
+
+    public static List<Company> findPage(IDPage page) throws SQLException {
+        boolean isAsc = "ASC".equals(page.getSortType());
+        Expression where = isAsc? new Expression("id").gt(page.getLastId()) : new Expression("id").lt(page.getLastId());
+        OrderBy orderBy = new OrderBy(new OrderItem("id",isAsc ? OrderItem.OrderType.ASC : OrderItem.OrderType.DESC));
+        Limit limit = new Limit(page.getPageSize());
+        Select.SqlCtrl sqlCtrl = new Select.SqlCtrl(orderBy,limit);
+        return getSelect().select(Company.class,where,sqlCtrl);
     }
 
 }
